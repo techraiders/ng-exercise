f4e81b65b82bc0aacf330b618df9f115e4288557
techraiders <hello_navneet@hotmail.com>
2017-02-05 02:05:20 +0530

    Understanding execution flow of compile, pre, post, controller of custom directives.

--- js/main.js
+++ js/main.js
@@ -1,33 +1,29 @@
-(function () {
-  angular.module('mainModule', ['ui.router','submodule'])
-  .config(['$stateProvider', function($stateProvider) {
-
-    $stateProvider
-      .state('state1', {
-        url: '/state1',
-        controller: 'paramCtrl as p',
-        template: '<h2> {{p.msg}} This is state 1. </h2> Go to <button ui-sref="state1.1"> state 1.1 </button> <br/> <ui-view></ui-view>'
-      })
-      .state('state1.1', {
-        url: '/state1.1',
-        controller: 'submoduleCtrl as vm',
-        template: ' {{p.msg}} {{vm.Name}}, This is state 1.1'
-      })
-      .state('state2', {
-        url: '/state2',
-        template: '<h2>This is state 2</h2>'
-      });
-  }])
-  .controller('paramCtrl', ['$state', function($state){
-    this.goToState = function(stateName) {
-      $state.go(stateName);
-    };
-      this.msg = 'hello';
-  }]);
-})();
-
 (function() {
-  angular.module('submodule', []).controller('submoduleCtrl', function(){
-    this.Name = 'NAVNEET';
-  });
+  angular.module('mainModule', ['ui.router'])
+    .controller('msg', ['$scope', msg])
+    .directive('message', message);
+
+  function msg($scope) {
+    $scope.msg = 'This is a message';
+  }
+
+  function message() {
+    return {
+      compile: function(tElement, tAttributes) {
+        console.log(tAttributes.text + " -In compile");
+        return {
+          pre: function(scope, iElement, iAttributes) {
+            console.info(iAttributes.text + ' -In Pre');
+          },
+          post: function(scope, iElement, iAttributes) {
+            console.log(iAttributes.text + ' -In Post');
+          }
+        };
+      },
+      controller: function($scope, $element, $attrs) {
+        console.log($attrs.text + ' -In controller');
+      }
+    };
+  }
+
 })();
