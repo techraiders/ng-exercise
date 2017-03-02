00059aeb434022eee7e36108638985a86d613751
navneet <hello_navneet@hotmail.com>
2017-03-02 19:41:05 +0530

    Separating controller definition out of the component.

--- ps-movies/movie-list.component.js
+++ ps-movies/movie-list.component.js
@@ -1,16 +1,18 @@
-(function() {
+(function () {
   'use strict';
   angular.module('psMovies')
     .component('movieList', {
       templateUrl: '/ps-movies/movie-list.component.html',
       controllerAs: 'vm',
-      controller: function () {
-        var vm = this;
-        vm.message = 'Hello from a component controller';
-
-        vm.changeMessage = function () {
-          vm.message = 'new changed messge';
-        };
-      }
+      controller: controller
     });
+
+  function controller() {
+    var vm = this;
+    vm.message = 'Hello from a component controller';
+    vm.changeMessage = function () {
+      vm.message = 'new changed messge';
+    };
+  }
 })();
+
