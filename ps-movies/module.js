d3ed8e3946deca4d3954f1b987f81e6eaf83c4e9
navneet <hello_navneet@hotmail.com>
2017-03-03 19:52:34 +0530

    Routing components with angular-router ngRoute version 1.5.3

--- ps-movies/module.js
+++ ps-movies/module.js
@@ -1,5 +1,20 @@
 (function() {
   'use strict';
-  angular.module('psMovies', []);
+  angular.module('psMovies', ["ngRoute"])
+    .component('appAbout', {
+      template: 'This is the about page'
+    })
+    .config(['$routeProvider', routeProvider]);
+
+    function routeProvider ($routeProvider) {
+      $routeProvider
+        .when("/list", {
+          template: "<movie-list></movie-list>"
+        })
+        .when("/about",{
+          template: "<app-about></app-about>"
+        })
+        .otherwise({redirectTo: "/list"});
+    }
 })();
 
