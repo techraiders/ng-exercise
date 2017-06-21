eb0a59e94fe68569ee3bf8a100a24d725102763f
navneet <hello_navneet@hotmail.com>
2017-06-22 01:46:38 +0530

    Injecting transcluded content into custom directing using ng-transclude directive.

--- js/main.js
+++ js/main.js
@@ -1,29 +1,14 @@
 (function() {
   angular.module('mainModule', ['ui.router'])
-    .controller('msg', ['$scope', msg])
-    .directive('message', message);
+    .controller('msgCtrl', function($scope) {
 
-  function msg($scope) {
-    $scope.msg = 'This is a message';
-  }
+    })
+    .directive('message1', function() {
+      return {
+        templateUrl: 'msg1.html',
+        transclude: true,
+      };
+    });
 
-  function message() {
-    return {
-      compile: function(tElement, tAttributes) {
-        console.log(tAttributes.text + " -In compile");
-        return {
-          pre: function(scope, iElement, iAttributes) {
-            console.info(iAttributes.text + ' -In Pre');
-          },
-          post: function(scope, iElement, iAttributes) {
-            console.log(iAttributes.text + ' -In Post');
-          }
-        };
-      },
-      controller: function($scope, $element, $attrs) {
-        console.log($attrs.text + ' -In controller');
-      }
-    };
-  }
 
 })();
