d0e9e2b063d89ae97b12d627673b0852d27025a5
prateek <prateekbhargava@hotmail.com>
2017-08-03 13:28:48 +0530

    Moves chart data source to controller.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 (function() {
   'use strict';
-  angular.module('mainModule', ['ui.router', 'dx']);
+  angular.module('mainModule', ['ui.router', 'dx'])
+    .controller('mainController', [function(){
+      var vm = this;
+      vm.fruits = [
+        { fruit: 'Oranges', total: 10 },
+        { fruit: 'Apples', total: 15 },
+        { fruit: 'Bananas', total: 9 }
+      ];
+    }]);
 })();
