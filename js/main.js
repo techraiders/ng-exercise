cf21837aa02e10334cf0d6d50fa5b0ab34af4670
prateek <prateekbhargava@hotmail.com>
2017-08-03 13:22:48 +0530

    Creates some basic charts.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,4 @@
 (function() {
-  angular.module('mainModule', ['ui.router'])
-    .controller('msgCtrl', function($scope) {
-
-    })
-    .directive('message1', function() {
-      return {
-        templateUrl: 'msg1.html',
-        transclude: true,
-      };
-    });
-
-
+  'use strict';
+  angular.module('mainModule', ['ui.router', 'dx']);
 })();
