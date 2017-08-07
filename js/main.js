d6d1a8768f593680591fd019c19ca6c568891add
navneet <hello_navneet@hotmail.com>
2017-08-07 22:04:07 +0530

    Demonstrates event binding in custom directive, binds mouseenter and mouseleave event on button.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,19 @@
 (function() {
+  'use strict';
   angular.module('mainModule', ['ui.router'])
-    .controller('msgCtrl', function($scope) {
-
-    })
-    .directive('message1', function() {
-      return {
-        templateUrl: 'msg1.html',
-        transclude: true,
-      };
+    .directive('interactiveBtn', function () {
+        return {
+            resctrict: 'A',
+            link: function (scope, element, attrs) {
+                element.on('mouseenter', function () {
+                    // console.log(element);
+                    element[0].innerText = 'Rolled Over';
+                });
+                element.on('mouseleave', function () {
+                    element[0].innerText = 'Rolled out';
+                });
+            }
+        };
     });
-
 
 })();
