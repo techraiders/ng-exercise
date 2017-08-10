8cd051843c76b3983d81556657b80378194f40f9
prateek <prateekbhargava@hotmail.com>
2017-08-10 11:17:59 +0530

    Demonstrates implementation of different type of texBoxes.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,53 @@
 (function() {
   'use strict';
   angular.module('mainModule', ['ui.router', 'dx'])
-    .controller('mainController', [function() {
+    .controller('mainController', [Controller]);
+
+    function Controller() {
       var vm = this;
-      vm.chartOptions = {
-        dataSource: [
-          { fruit: 'Oranges', total: 10 },
-          { fruit: 'Apples', total: 15 },
-          { fruit: 'Bananas', total: 9 }
-        ],
-        series: { argumentField: 'fruit', valueField: 'total' }
+
+      vm.textBox = {
+        simple: {
+          value: 'John Smith'
+        },
+        withPlaceholder: {
+          value: 'Enter full name here...'
+        },
+        withClearButton: {
+          value: 'John Smith',
+          showClearButton: true
+        },
+        passwordMode: {
+          mode: 'password',
+          placeholder: 'Enter password',
+          showClearButton: true,
+          value: 'kjdf57sjh'
+        },
+        maskUsage: {
+            mask: "+1 (X00) 000-0000",
+            maskRules: {"X": /[02-9]/}
+        },
+        disabled: {
+            value: "John Smith",
+            disabled: true
+        },
+        fullName: {
+            value: "Smith",
+            showClearButton: true,
+            placeholder: "Enter full name",
+            valueChangeEvent: "keyup",
+            onValueChanged: function(data) {
+              console.log(data);
+                vm.emailValue = data.value.replace(/\s/g, "").toLowerCase() + "@corp.com";
+            }
+        },
+        email: {
+            readOnly: true,
+            hoverStateEnabled: false,
+            bindingOptions: {
+                value: "vm.emailValue"
+            }
+        }
       };
-    }]);
+    }
 })();
