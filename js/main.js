d427862ac68a4c6d91d571b7461ee65935c2d0af
navneet <hello_navneet@hotmail.com>
2017-08-13 22:00:39 +0530

    Adds date some filters.

--- js/main.js
+++ js/main.js
@@ -1,28 +1,8 @@
 (function() {
   'use strict';
   angular.module('mainModule', ['ui.router'])
-    .directive('interactiveBtn', function () {
-        return {
-            resctrict: 'A',
-            link: function (scope, element, attrs) {
-                element.on('mouseenter', function () {
-                    // console.log(element);
-                    element[0].innerText = 'Rolled Over';
-                    element.css({
-                        backgroundColor: 'blue',
-                        color: 'white'
-                    });
-                });
-                element.on('mouseleave', function () {
-                    element[0].innerText = 'Rolled out';
-                    console.log(element);
-                    element.css({
-                        backgroundColor: 'yellow',
-                        color: 'black'
-                    });
-                });
-            }
-        };
+    .controller('DemoController', function () {
+      this.date = new Date();
     });
 
 })();
