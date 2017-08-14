b8424c280049a0341bb52f1233cbb2e6748d5888
navneet <hello_navneet@hotmail.com>
2017-08-14 13:41:25 +0530

    Parent component requires child-component and child executes funtion of parent.

--- js/main.js
+++ js/main.js
@@ -6,17 +6,25 @@
     transclude: true,
     template: `
       <div ng-transclude></div>
-    `
+    `,
+    controller: function () {
+      this.foo = function () {
+        return 'Foo from parent!';
+      };
+    }
   });
 
   app.component('childComponent', {
     bindings: {
       count: '='
     },
+    require: {
+      parent: '^parentComponent'
+    },
     controller: function () {
       this.state = 'Not loaded';
       this.$onInit = function () {
-        this.state = 'Loaded!';
+        this.state = this.parent.foo();
       };
     },
     template: `
