911472633398fc34261418126327a39246e74fa0
navneet <hello_navneet@hotmail.com>
2017-08-14 12:29:21 +0530

    Sets up parent and child component.

--- js/main.js
+++ js/main.js
@@ -1,45 +1,29 @@
 (function() {
+  'use strict';
   var app = angular.module('app', []);
 
-  /*function AccordianController() {
-    var vm = this;
-
-    // add panel
-    vm.addPanel = function() {
-      // code to add panel
-    };
-
-    vm.selectPanel = function() {
-      // code to select panel
-    };
-  }
-
-  // register the accordian component
-  app.component('accordian', {
-    template: '<!-- accordian-template -->',
-    controller: AccordianController
+  app.component('parentComponent', {
+    transclude: true,
+    template: `
+      <div ng-transclude></div>
+    `
   });
 
-  function AccordianPanelController() {
-    // use parents methods here
-    var vm = this;
-
-    // add panel
-    vm.parent.addPanel(vm);
-
-    // select panel
-    vm.parent.selectPanel(vm);
-  }
-
-  // register the accordian-panel component
-  app.component('accordianPanel', {
-    // require the parent component
-    // In this case parent is an instance of accordian component
-    require: {
-      'parent' : '^accordian',
-      template: '<!-- accordian-panel-template -->',
-      controller: AccordianController
-    }
-  });*/
+  app.component('childComponent', {
+    bindings: {
+      count: '='
+    },
+    controller: function () {
+      this.state = 'Not loaded';
+      this.$onInit = function () {
+        this.state = 'Loaded!';
+      };
+    },
+    template: `
+      <div>
+        component: {{$ctrl.state}}
+      </div>
+    `
+  });
 
 })();
