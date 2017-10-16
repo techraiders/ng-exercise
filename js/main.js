cd3bbfadb2e5f475943cb262bd164b07bf151d8f
Navneet Prakash <hello_navneet@hotmail.com>
2017-10-16 22:18:16 +0530

    Configures provider service in app.config

--- js/main.js
+++ js/main.js
@@ -1,16 +1,32 @@
 (function () {
   var app = angular.module('mainModule', []);
-  app.provider('books', function  () {
-      this.$get = function  () {
+  app.provider('books', function () {
+      this.$get = function () {
         var appName = 'Book Logger';
         var appDesc = 'Track which books you read.';
+
+        var version = '1.0';
+
+        if (includeVersionInTitle) {
+          appName += ' ' + version;
+        };
 
         return {
           appName: appName,
           appDesc: appDesc
         };
       };
+
+      var includeVersionInTitle = false;
+      this.setIncludeVersionInTitle = function (value) {
+        includeVersionInTitle = value;
+      };
+
     });
+
+  app.config(function  (booksProvider) {
+    booksProvider.setIncludeVersionInTitle(true);
+  });
 
   app.controller('BooksController', function (books) {
     var vm = this;
