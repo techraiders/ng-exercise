47d64a06d50221cbff40bcbd70a3f879de2e34d0
Navneet Prakash <hello_navneet@hotmail.com>
2017-10-16 21:56:36 +0530

    Creates a service with $provide.

--- js/main.js
+++ js/main.js
@@ -1,33 +1,21 @@
 (function () {
-  angular.module('mainModule', ['ui.router','submodule'])
-  .config(['$stateProvider', function($stateProvider) {
+  var app = angular.module('mainModule', []);
+  app.config( function  ($provide) {
+    $provide.provider('books', function  () {
+      this.$get = function  () {
+        var appName = 'Book Logger';
+        var appDesc = 'Track which books you read.';
 
-    $stateProvider
-      .state('state1', {
-        url: '/state1',
-        controller: 'paramCtrl as p',
-        template: '<h2> {{p.msg}} This is state 1. </h2> Go to <button ui-sref="state1.1"> state 1.1 </button> <br/> <ui-view></ui-view>'
-      })
-      .state('state1.1', {
-        url: '/state1.1',
-        controller: 'submoduleCtrl as vm',
-        template: ' {{p.msg}} {{vm.Name}}, This is state 1.1'
-      })
-      .state('state2', {
-        url: '/state2',
-        template: '<h2>This is state 2</h2>'
-      });
-  }])
-  .controller('paramCtrl', ['$state', function($state){
-    this.goToState = function(stateName) {
-      $state.go(stateName);
-    };
-      this.msg = 'hello';
-  }]);
-})();
-
-(function() {
-  angular.module('submodule', []).controller('submoduleCtrl', function(){
-    this.Name = 'NAVNEET';
+        return {
+          appName: appName,
+          appDesc: appDesc
+        };
+      };
+    });
   });
-})();
+
+  app.controller('BooksController', function (books) {
+    var vm = this;
+    vm.appName = books.appName;
+  });
+} () );
