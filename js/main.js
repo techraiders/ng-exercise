c8dbb9f70600d8da2f72269fd8d33ce80f688dc7
Navneet Prakash <hello_navneet@hotmail.com>
2017-10-24 20:11:08 +0530

    Adds or removes methods on a service using decorators.

--- js/main.js
+++ js/main.js
@@ -2,18 +2,25 @@
 (function () {
   var app = angular.module('mainModule', []);
 
-  app.config(function  ($provide) {
+  app.config(function  ($provide) {  	
   	$provide.decorator('$log', function  ($delegate) {
   		return {
-  			log: function  (message) {
+  			/*log: function  (message) {
   				$delegate.log(message.toUpperCase());
+  			},*/
+  			awesome: function  (message) {
+  				$delegate.info(message);
   			}
   		};
   	});
   });
 
   app.controller('BooksController', function  ($log) {
-  	$log.log('this message is in lower case but will be transformed to UPPERCASE because of its decorator.');
+  	// $log.log('this message is in lower case but will be transformed to UPPERCASE because of its decorator.');
+
+  	console.log($log);
+
+  	$log.awesome('Hi, I am printed by awesome method, attched to $log service by decorating it.');
   });
   
 } () );
