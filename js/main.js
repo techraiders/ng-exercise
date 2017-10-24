4b62f9c9c7ac8b67eae2582811e92c923cae67ca
Navneet Prakash <hello_navneet@hotmail.com>
2017-10-24 19:38:11 +0530

    decorates log method of log service to log message in uppercase.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,19 @@
+'use strict';
 (function () {
-  var app = angular.module('mainModule', ['ngCookies']);
-  
-  app.controller('BooksController', function ($cookies, $cookieStore) {
-    var vm = this;
-    
-    vm.setAsFavorite = function (currentBook) {
-      $cookies.put('favoriteBook', currentBook);
-      console.log($cookies.get('favoriteBook'));
-    };
+  var app = angular.module('mainModule', []);
 
-    
+  app.config(function  ($provide) {
+  	$provide.decorator('$log', function  ($delegate) {
+  		return {
+  			log: function  (message) {
+  				$delegate.log(message.toUpperCase());
+  			}
+  		};
+  	});
   });
+
+  app.controller('BooksController', function  ($log) {
+  	$log.log('this message is in lower case but will be transformed to UPPERCASE because of its decorator.');
+  });
+  
 } () );
