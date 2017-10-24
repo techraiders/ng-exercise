2913a6a7be07c354c99b202aebc7c89b272f8abb
Navneet Prakash <hello_navneet@hotmail.com>
2017-10-24 19:02:19 +0530

    Sets cookies using $cookies service get, and put method.

--- js/main.js
+++ js/main.js
@@ -1,35 +1,14 @@
 (function () {
-  var app = angular.module('mainModule', []);
-  app.provider('books', function () {
-      this.$get = function () {
-        var appName = 'Book Logger';
-        var appDesc = 'Track which books you read.';
-
-        var version = '1.0';
-
-        if (includeVersionInTitle) {
-          appName += ' ' + version;
-        };
-
-        return {
-          appName: appName,
-          appDesc: appDesc
-        };
-      };
-
-      var includeVersionInTitle = false;
-      this.setIncludeVersionInTitle = function (value) {
-        includeVersionInTitle = value;
-      };
-
-    });
-
-  app.config(function  (booksProvider) {
-    booksProvider.setIncludeVersionInTitle(true);
-  });
-
-  app.controller('BooksController', function (books) {
+  var app = angular.module('mainModule', ['ngCookies']);
+  
+  app.controller('BooksController', function ($cookies, $cookieStore) {
     var vm = this;
-    vm.appName = books.appName;
+    
+    vm.setAsFavorite = function (currentBook) {
+      $cookies.put('favoriteBook', currentBook);
+      console.log($cookies.get('favoriteBook'));
+    };
+
+    
   });
 } () );
