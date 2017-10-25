838b28288fd0fa944e2890a307a9e7f63b0be1e3
Navneet Prakash <hello_navneet@hotmail.com>
2017-10-25 19:19:53 +0530

    Registers a provider and avails to controller.

--- js/main.js
+++ js/main.js
@@ -1,26 +1,24 @@
-'use strict';
-(function () {
-  var app = angular.module('mainModule', []);
+var app = angular.module('mainModule', []);
 
-  app.config(function  ($provide) {  	
-  	$provide.decorator('$log', function  ($delegate) {
-  		return {
-  			/*log: function  (message) {
-  				$delegate.log(message.toUpperCase());
-  			},*/
-  			awesome: function  (message) {
-  				$delegate.info(message);
-  			}
-  		};
-  	});
-  });
+app.config(function  ($provide) {  	
+	$provide.decorator('$log', function  ($delegate) {
+		return {
+			/*log: function  (message) {
+				$delegate.log(message.toUpperCase());
+			},*/
+			awesome: function  (message) {
+				$delegate.info(message);
+			}
+		};
+	});
+});
 
-  app.controller('BooksController', function  ($log) {
-  	// $log.log('this message is in lower case but will be transformed to UPPERCASE because of its decorator.');
+app.controller('BooksController', function  ($log, books) {
+	// $log.log('this message is in lower case but will be transformed to UPPERCASE because of its decorator.');
 
-  	console.log($log);
+	//console.log($log);
 
-  	$log.awesome('Hi, I am printed by awesome method, attched to $log service by decorating it.');
-  });
-  
-} () );
+	$log.awesome('Hi, I am printed by awesome method, attched to $log service by decorating it.');
+
+  console.log(books);
+});
---
+++ js/provide.js
@@ -0,0 +1,12 @@
+app.config(function  ($provide) {
+	$provide.provider('books', function  () {
+		this.$get = function  () {
+			var appName = 'Book Logger';
+
+			return {
+				appName: appName
+			};
+		}
+	});
+});
+
